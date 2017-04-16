10fd14d7559aa8452b673804f2e3f757fed5e340
Chihiro <Chihiro@M.local>
2017-04-16 22:10:32 +0900

    SecondKadaiApp

---
+++ ResultViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Chihiro Murakami on 2017/04/16.
+//  Copyright © 2017年 chihiro.murakami. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+    @IBOutlet weak var label: UILabel!
+    
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var name:Int = 0　//????
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //表示する文章
+    label.text = "こんにちは、 \(name) さん"
+}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+    
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+    }
 
-
+    weak var name: UITextField! {
+        
+        // UITextFieldを作成する.
+        name = UITextField(frame: CGRectMake(0,0,200,30))
+        // 表示する文字を代入する.
+        name.text = "文字を入力してください"
+        name.text!
+    }
+    
 }
 
