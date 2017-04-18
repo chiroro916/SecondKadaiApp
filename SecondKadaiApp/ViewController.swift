bbeae51c9760c955a20831dddcf411b12aa0d063
Chihiro <Chihiro@M.local>
2017-04-18 22:04:32 +0900

    placeholderの追加。textfieldのサイズ固定。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -15,8 +15,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        name.placeholder = "文字を入力してください"
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
